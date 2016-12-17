edd2e51d4fee51c5c97909c4a49c9ad22f47570a
hehe <hehelemon@outlook.com>
2016-12-17 20:39:35 +0800

    PSO 二次函数算例

--- Math/math.py
+++ Math/math.py
@@ -1,14 +1,17 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
 def dd(tup_x, tup_y):
     n = len(tup_x)
+    m=[0]*n
+    for i in range(0, n):
+        m[i] = tup_y[i];
     for i in range(1, n):
         j = n - 1;
         while j >= i:
-            tup_y[j] = (tup_y[j] - tup_y[j - 1]) / (tup_x[j] - tup_x[j - i])
+            m[j] = (m[j] - m[j - 1]) / (tup_x[j] - tup_x[j - i])
             j -= 1
-    return tup_y
+    return m
 
 
 def ni(xxArray,tup_x,tup_y):
@@ -25,15 +28,7 @@ def ni(xxArray,tup_x,tup_y):
     return yyArray
 
 def tss(a, b, c, d, n, x):
-    """
-    本算法解三对角线方程组，系数存于数组a、b、c中
-    :param a:对角线矩阵系数
-    :param b:对角线矩阵系数
-    :param c:对角线矩阵系数
-    :param d:右端向量
-    :param n:
-    :param x:
-    """
+
     u = [0] * n
     y = [0] * n
     l = [0] * n
@@ -52,13 +47,7 @@ def tss(a, b, c, d, n, x):
 
 
 def find_k(x, _x):
-    """
-    用于找出_x所在的区间，以X[i]的下标值I为返回结果
-    :param x: 插值点数组
-    :param n: 插值点数组的大小
-    :param _x: 待计算的x值
-    :return: 区间的坐标，如果没有符合要求的区间，返回-1
-    """
+
     n = len(x)
     for i in range(0, n - 1):
         if (_x <= x[i]):
@@ -67,17 +56,8 @@ def find_k(x, _x):
 
 
 def splinem(x, y, lambda_0, d_0, u_n, d_n):
-    """
 
-    :param x: 插值点X坐标数组
-    :param y: 插值点Y坐标数组
-    :param lambda_0: 边界条件
-    :param d_0: 边界条件
-    :param u_n: 边界条件
-    :param d_n: 边界条件
-    """
-
-    n = len(x);  # 这里的n是只数组下标的最大取值，所以是数组大小-1
+    n = len(x);
     M = [0] * (n);
     h = [0] * (n + 1)
     a = [0] * (n)
@@ -90,7 +70,7 @@ def splinem(x, y, lambda_0, d_0, u_n, d_n):
         while (i > (k-1)):
             M[i] = (M[i] - M[i - 1]) / (x[i] - x[i - 1])
             i -= 1
-    h[1] = x[1] - x[0]  # 给h1赋值
+    h[1] = x[1] - x[0]
     for i in range(1, n - 1):
         h[i + 1] = x[i + 1] - x[i]
         c[i] = h[i + 1] / (h[i] + h[i + 1])
@@ -131,16 +111,30 @@ def printArrayASJson(x, y):
     for i in range(0,n):
         print("[" + str(x[i]) + "," + str(y[i]) + "],")
 
-y = [9.01, 8.96, 7.96, 7.97, 8.02, 9.05, 10.13, 11.18, 12.26, 13.28, 13.32, 12.61, 11.29, 10.22, 9.15, 7.90,
-             7.95, 8.86, 9.81, 10.80, 10.93]
-x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-printArrayASJson(x,y)
-xx=0
-xxArray =[]
-while xx<=20:
-    xxArray.append(xx)
-    xx+=0.5
-yyArray = evaspline(xxArray,x,y)
-printArrayASJson(xxArray,yyArray)
-yyArray = ni(xxArray,x,y)
-# printArrayASJson(xxArray,yyArray)
+def question1():
+
+    y = [9.01, 8.96, 7.96, 7.97, 8.02, 9.05, 10.13, 11.18, 12.26, 13.28, 13.32, 12.61, 11.29, 10.22, 9.15, 7.90,
+                 7.95, 8.86, 9.81, 10.80, 10.93]
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    printArrayASJson(x,y)
+
+    xx=0
+    xxArray =[]
+    while xx<=20:
+        xxArray.append(xx)
+        xx+=0.01
+
+    yyArray = evaspline(xxArray,x,y)
+
+    yyArray = ni(xxArray,x,y)
+    #printArrayASJson(xxArray,yyArray)
+
+    print(np.array(y))
+    plt.figure(figsize=(8,4))
+    plt.plot(np.array(xxArray),np.array(yyArray),label="$ni$",color="green")
+    plt.plot(np.array(x),np.array(y),'*',label="$data$",color="red")
+    plt.ylim(0,20)
+    plt.legend()
+    plt.show()
+
+question1()
